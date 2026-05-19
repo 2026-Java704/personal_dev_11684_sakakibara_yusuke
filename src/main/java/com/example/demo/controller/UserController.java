ceeb675763414a3ff078b3e800c35dd62fef1f10
student <student@FJ-1025>
2026-05-19 17:13:11 +0900

    ログインのコントローラと表示
    次の最初にやる事
    UserのEntityに@Columを付ける

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,91 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.Account;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+public class UserController {
+	private final HttpSession session;
+	private final Account account;
+	private final UserRepository userRepository;
+
+	public UserController(
+			HttpSession session,
+			Account account,
+			UserRepository userRepository) {
+		this.session = session;
+		this.account = account;
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping({ "/login" })
+	public String index() {
+
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(
+			@RequestParam String name,
+			@RequestParam String password,
+			Model model) {
+
+		if (name.length() == 0 || password.length() == 0) {
+			model.addAttribute("message", "ユーザー名とパスワードを入力してください");
+			return "login";
+		}
+
+		List<User> userList = userRepository.findByNameAndPassword(name, password);
+		if (userList == null || userList.size() == 0) {
+			model.addAttribute("message", "ユーザー名かパスワードが違います");
+			return "login";
+		}
+		User user = userList.get(0);
+
+		account.setName(user.getName());
+
+		return "redirect:/medicine";
+	}
+
+	@GetMapping("/account")
+	public String create() {
+		return "accountForm";
+	}
+
+	@PostMapping("/account")
+	public String store(
+			@RequestParam String name,
+			@RequestParam String password,
+			Model model) {
+
+		List<String> errorList = new ArrayList<>();
+		if (name.length() == 0) {
+			errorList.add("名前は必須です");
+		}
+		if (password.length() == 0) {
+			errorList.add("パスワードは必須です");
+		}
+		if (errorList.size() > 0) {
+			model.addAttribute("errorList", errorList);
+			model.addAttribute("name", name);
+			model.addAttribute("password", password);
+			return "accountForm";
+		}
+		User user = new User(name, password);
+		userRepository.save(user);
+		return "redirect:/login";
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String password;
+
+	public User() {
+	}
+
+	public User(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
