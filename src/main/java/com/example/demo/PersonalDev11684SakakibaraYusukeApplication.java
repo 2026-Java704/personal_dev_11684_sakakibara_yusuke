058d16045b5ca88028ee39005b5a3ecc29406554
student <student@FJ-1025>
2026-05-19 14:09:06 +0900

    新規

---
+++ src/main/java/com/example/demo/PersonalDev11684SakakibaraYusukeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11684SakakibaraYusukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11684SakakibaraYusukeApplication.class, args);
+	}
+
+}
