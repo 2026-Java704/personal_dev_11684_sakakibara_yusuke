8806fefd24991698cd4ed3c376c3b8131286def2
student <student@FJ-1025>
2026-05-20 17:50:53 +0900

    更新画面の追加

--- src/main/java/com/example/demo/controller/MedicineController.java
+++ src/main/java/com/example/demo/controller/MedicineController.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Medicine;
 import com.example.demo.model.Account;
@@ -36,4 +39,29 @@ public class MedicineController {
 		return "medicine";
 	}
 
+	@GetMapping("/medicine/{id}/edit")
+	public String edit(
+			@PathVariable Integer id,
+			Model model) {
+		Medicine medicine = medicineRepository.findById(id).get();
+		model.addAttribute("medicine", medicine);
+		return "editMedicine";
+	}
+
+	@PostMapping("/medicine/{id}/edit")
+	public String update(
+			@PathVariable Integer id,
+			@RequestParam(defaultValue = "") String name,
+			@RequestParam(defaultValue = "") String note,
+			@RequestParam(defaultValue = "") Integer count,
+			@RequestParam(defaultValue = "") Boolean mcheck) {
+		Medicine medicine = medicineRepository.findById(id).get();
+		medicine.setName(name);
+		medicine.setNote(note);
+		medicine.setCount(count);
+		medicine.setMcheck(mcheck);
+
+		medicineRepository.save(medicine);
+		return "redirect:/medicine";
+	}
 }
