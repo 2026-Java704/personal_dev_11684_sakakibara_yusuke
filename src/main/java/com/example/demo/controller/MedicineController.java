bf8931960d128765546117ad05640c52d0fabd71
student <student@FJ-1025>
2026-05-20 11:45:26 +0900

    MedicineのController,Entity,Repositoryの作成

--- src/main/java/com/example/demo/controller/MedicineController.java
+++ src/main/java/com/example/demo/controller/MedicineController.java
@@ -1,11 +1,29 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Medicine;
+import com.example.demo.repository.MedicineRepository;
 
 @Controller
 public class MedicineController {
+	private final MedicineRepository medicineRepository;
 
-	//	@GetMapping("/medicine")
-	//	public String index() {}
+	public MedicineController(
+			MedicineRepository medicineRepository) {
+		this.medicineRepository = medicineRepository;
+	}
+
+	@GetMapping("/medicine")
+	public String index(Model model) {
+		List<Medicine> medicineList = medicineRepository.findAll();
+		model.addAttribute("medicine", medicineList);
+
+		return "medicine";
+	}
 
 }
---
+++ src/main/java/com/example/demo/entity/Medicine.java
@@ -0,0 +1,75 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medicine")
+public class Medicine {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String note;
+	private Integer count;
+	@Column(name = "m_check")
+	private Boolean mcheck;
+	@ManyToOne
+	@JoinColumn(name = "users_id")
+	private User user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public Boolean getMcheck() {
+		return mcheck;
+	}
+
+	public void setMcheck(Boolean mcheck) {
+		this.mcheck = mcheck;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
